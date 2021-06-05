d8beae932a36a9b717b748f9777790e449a318f1
Berk OZMENEKSE <ozmenekseberk@gmail.com>
2021-06-05 04:25:02 +0300

    Added Dtos package

--- hrms/src/main/java/hrmsproject/hrms/dataAccess/abstracts/CandidateDao.java
+++ hrms/src/main/java/hrmsproject/hrms/dataAccess/abstracts/CandidateDao.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import hrmsproject.hrms.entities.concretes.Candidate;
 
+
 public interface CandidateDao extends JpaRepository<Candidate, Integer> {
 	
 	
---
+++ hrms/src/main/java/hrmsproject/hrms/entities/dtos/CurriculumVitaeDto.java
@@ -0,0 +1,31 @@
+package hrmsproject.hrms.entities.dtos;
+
+import java.util.List;
+
+import hrmsproject.hrms.entities.concretes.Candidate;
+import hrmsproject.hrms.entities.concretes.CandidateAbility;
+import hrmsproject.hrms.entities.concretes.CandidateEducation;
+import hrmsproject.hrms.entities.concretes.CandidateExperience;
+import hrmsproject.hrms.entities.concretes.CandidateLanguage;
+import hrmsproject.hrms.entities.concretes.CandidateLink;
+import hrmsproject.hrms.entities.concretes.Image;
+import hrmsproject.hrms.entities.concretes.LetterOfApproval;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurriculumVitaeDto {
+	private Candidate candidate;
+	private Image image;
+	private List<CandidateLink> links;
+	private List<CandidateLanguage> languages;
+	private List<CandidateAbility> abilities;
+	private List<CandidateEducation> educations;
+	private List<LetterOfApproval> letters;
+	private List<CandidateExperience> experiences;
+	
+}
